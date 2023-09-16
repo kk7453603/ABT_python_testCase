8590956b10e7a8aa9c3bf52f609881da0ba84d06
kirillkom <kk7453603@gmail.com>
2023-09-17 01:17:36 +0300

    Задание 1 сделано

--- 1.py
+++ 1.py
@@ -1,10 +1,24 @@
 import os
-
+from math import ceil
 
 def black_book(page: int) -> bool:
     status_code = os.system(f"./black-book -n {page}")
     return status_code == 0
 
+"""Странное условие с последней страницей. Как я понял, True означает наличие страницы в книге, а False - 
+отсутствие, на этом и строил свой алгоритм"""
+def find_last_page(): #бинарный поиск наиболее быстрый алгоритм для поиска упорядоченных чисел на интервале
+    lower_bound = 1
+    upper_bound = 10000000 # so much
+
+    while lower_bound < upper_bound:
+        mid = ceil((lower_bound + upper_bound) / 2)
+        if black_book(mid):
+            lower_bound = mid
+        else:
+            upper_bound = mid - 1
+
+    return lower_bound
 
 def main():
     """
@@ -24,7 +38,8 @@ def main():
     Важно: написать наиболее эффективный алгоритм (по числу итераций)
     """
     # тут явно нужен алгоритм
-
+    last_page = find_last_page()
+    print("Номер последней страницы:", last_page)
 
 
 if __name__ == '__main__':
