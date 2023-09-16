d37fa62ee510a7f892bd1cfc1c4b8997fdf06aef
kirillkom <kk7453603@gmail.com>
2023-09-16 20:35:03 +0300

    задача 3 сделана

--- 3.py
+++ 3.py
@@ -17,30 +17,34 @@ class SharedState:
         self.items.append(value)
 
 
+
 # Имитация сервера, обрабатывающего запросы
 # В нашем случае "запросы" модифицируют состояние сервера
 # добавляя элементы в конец списка 'items'
 class Server:
     state: SharedState
-
-    def __init__(self, state: SharedState):
+    semaphore: asyncio.Semaphore # Добавляем семафор для формирования очереди
+    def __init__(self, state: SharedState, semaphore: asyncio.Semaphore):
         self.state = state
+        self.semaphore = semaphore
 
     async def handle_request(self, value: int):
-        await self.state.modify(value)
+        async with self.semaphore: # Производим захват семафора перед модификацией состояния
+            await self.state.modify(value)
 
 
 async def main():
     state = SharedState()
-    server = Server(state)
+    semaphore = asyncio.Semaphore(1)  # Создаем семафор с единичной емкостью для создания очереди друг за другом
+    server = Server(state,semaphore)
+
 
     # имитируем запуск 10 запросов к серверу
     requests = [server.handle_request(value) for value in range(10)]
     await asyncio.gather(*requests)
 
     '''
-    !!! В данной задаче нельзя модифицировать код - только добавлять новый !!!
-    
+    !!! В данной задаче нельзя модифицировать код - только добавлять новый !!!   
     задача заключается в том, чтобы только средствами asyncio
     заставить запросы работать последовательно (исключить data race)
     state в результате обработки запросов должен удовлетворять следующему условию:
